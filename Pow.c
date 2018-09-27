d57b7d260597964ecf0676b83c59b666c11fceae
amilaweesinghe <amilaweerasinghe677@gmail.com>
2018-09-27 16:38:59 +0530

    Power

---
+++ Pow.c
@@ -0,0 +1,37 @@
+// Week10_Pow.c
+// Compute the nth power of x, where
+// n is a non-negative integer.
+#include <stdio.h>
+
+double mypow(double, int);
+
+int main(void) {
+	double x;
+	int n;
+
+	printf("Enter x and n: ");
+	scanf("%lf %d", &x, &n);
+
+	printf("mypow(%f, %d) = %f\n", x, n, mypow(x, n));
+	return 0;
+}
+
+// Compute the nth power of x.
+// Precond: n >= 0
+double mypow(double x, int n) {
+	/*if(n==0){
+	return 1;
+	         }         this method can be used too,less reduncancy
+	if(n%2==0){
+	           return mypow(x*x,n/2);
+	           }
+	           return mypow(x*x,n/2)*x;  */     // to be completed
+	           if(n==0){    
+	           return 1;  //known initial condition
+	           }
+	           else{
+	           return x*mypow(x,n-1);// return x*x^n-1
+	           }
+
+}
+
